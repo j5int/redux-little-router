a4ce98bbc09559ef099f15a5e9066e91d61a9db5
David Fraser <davidf@j5int.com>
2016-12-12 09:49:41 +0200

    Resolve duplicate merged methods of finding currentLocation

--- src/link.js
+++ src/link.js
@@ -108,11 +108,9 @@ const Link = (
   } = props;
 
   const { router } = context;
-  const { router: currentLocation } = router.store.getState();
-  const { basename } = currentLocation;
-
   const state = router.store.getState();
   const currentLocation = state.get ? state.get('router') : state.router;
+  const { basename } = currentLocation;
 
   const locationDescriptor =
     resolveQueryForLocation({
